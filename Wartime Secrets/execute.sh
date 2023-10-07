e9e8ef2f2e321f262a91127b8506cf5557853833
Vidusha Sanidu <vidushsanidu07@gmail.com>
2023-10-07 11:12:25 +0530

    second commit

---
+++ Wartime Secrets/execute.sh
@@ -0,0 +1,24 @@
+checkStrength(){
+    
+    local password="$1"
+
+    if [ ${#password} -lt 8 ]; then
+        return false
+    fi
+
+    if ! [[ "$password" =~ [A-Z] ]]; then
+        return false
+    fi
+
+    if ! [[ "$password" =~ [a-z] ]]; then
+        return false
+    fi
+
+    if ! [[ "$password" =~ [0-9] ]]; then
+        return false
+    fi
+
+    return true
+}
+
+checkStrength "$1"
